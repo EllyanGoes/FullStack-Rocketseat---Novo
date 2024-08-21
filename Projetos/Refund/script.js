d8dd64384706de343fe96a8b7d4de5e4c4ac0146
Ellyan Góes <ellyan.goes@gmail.com>
2024-08-21 17:25:45 -0300

    Fist Commit

--- Projetos/Refund/script.js
+++ Projetos/Refund/script.js
@@ -2,6 +2,19 @@
 
 const amount = document.getElementById('amount')
 
+
+//Captura o evento de input para formatar o valor
 amount.oninput = () => {
-  console.log('novo conteúdo')
+  //obtem o valor atual do input e remove os caracteres não numéricos
+  let value = amount.value.replace(/\D/g,"");
+
+  //Atualiza o valor do input
+  amount.value = formatCurrencyBRL(value)
+ 
+}
+
+function formatCurrencyBRL(value){
+  value = value.toLocaleString("pt-BR", {
+    
+  })
 }
